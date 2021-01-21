9b5392aabb80e1fb03e12dfcf452c9e95f821dab
jfamaral <33514632+jfamaral@users.noreply.github.com>
2021-01-21 18:29:38 +0000

    Update __init__.py

--- skodaconnect/__init__.py
+++ skodaconnect/__init__.py
@@ -1,4 +1,4 @@
-from skodaconnect.api import Connection
-#from skodaconnect.dashboard import Dashboard
+from seatconnect.api import Connection
+#from seatconnect.dashboard import Dashboard
 
-__version__ = "1.0.30"
+__version__ = "1.0.0"
